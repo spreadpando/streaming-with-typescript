5106daa09196b98b7f82ee8dc9bc366ab78da9c9
pando <spreadpando@gmail.com>
2021-05-06 21:58:10 -0600

    any

--- components/contactForm/index.tsx
+++ components/contactForm/index.tsx
@@ -70,7 +70,7 @@ interface Inputs {
 const Contact: React.FC = () => {
   const [submitted, setSubmitted] = useState(false)
   const { register, handleSubmit } = useForm<Inputs>()
-  const onSubmit: Promise<NextApiResponse> = async (data: Inputs) => {
+  const onSubmit: Promise<NextApiResponse<any>> = async (data: Inputs) => {
     console.log('submitted')
     const res = await fetch('/api/contact', { // eslint-disable-line
       method: 'POST',
