3d4bc96413902f0b8ec8b2ec0c5eb5c443dd3a58
pando <spreadpando@gmail.com>
2021-05-19 11:12:09 -0600

    e type

--- components/player/index.tsx
+++ components/player/index.tsx
@@ -140,7 +140,7 @@ const Player: React.FC = () => {
     }
   }
 
-  const seek = (e, isClick: boolean): void => {
+  const seek = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, isClick: boolean): void => {
     if (scrubActive || isClick) {
       const position = e.clientX / scrubArea.current.getBoundingClientRect().width
       const percentage = 100 * position
